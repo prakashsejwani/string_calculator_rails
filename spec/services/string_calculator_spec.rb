c8d2d29e7b812371295e5cec6922d886cd8abfe8
Prakash Sejwani <prakashsejwani@gmail.com>
2025-10-20 17:49:47 +0530

    spec: support newlines between numbers

--- spec/services/string_calculator_spec.rb
+++ spec/services/string_calculator_spec.rb
@@ -23,5 +23,10 @@ RSpec.describe 'StringCalculator' do
       calculator = StringCalculator.new
       expect(calculator.add('1,2,3,4,5')).to eq(15)
     end
+
+    it 'supports new lines between numbers' do
+      calculator = StringCalculator.new
+      expect(calculator.add("1\n2,3")).to eq(6)
+    end
   end
 end
