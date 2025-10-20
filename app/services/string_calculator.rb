ca741dab0762a3c258ad6de973fdb317484e56c3
Prakash Sejwani <prakashsejwani@gmail.com>
2025-10-20 17:51:16 +0530

    feat: support custom single-char delimiter with //; prefix

--- app/services/string_calculator.rb
+++ app/services/string_calculator.rb
@@ -12,7 +12,17 @@ class StringCalculator
 
   def add(numbers)
     return 0 if numbers == ""
-    return numbers.to_i if numbers !~ DEFAULT_DELIMITERS
+    return numbers.to_i if numbers !~ DEFAULT_DELIMITERS && !numbers.start_with?("//")
+
+    if numbers.start_with?("//")
+      header, body = numbers.split("\n", 2)
+      # Simple custom delimiter form: //;\n
+      if header[2] && header[2] != '['
+        custom_delimiter = header[2]
+        return body.split(custom_delimiter).map(&:to_i).sum
+      end
+    end
+
     numbers.split(DEFAULT_DELIMITERS).map(&:to_i).sum
   end
 end
--- spec/services/string_calculator_spec.rb
+++ spec/services/string_calculator_spec.rb
@@ -28,5 +28,10 @@ RSpec.describe 'StringCalculator' do
       calculator = StringCalculator.new
       expect(calculator.add("1\n2,3")).to eq(6)
     end
+
+    it 'supports a custom single-char delimiter defined with //; prefix' do
+      calculator = StringCalculator.new
+      expect(calculator.add("//;\n1;2")).to eq(3)
+    end
   end
 end
