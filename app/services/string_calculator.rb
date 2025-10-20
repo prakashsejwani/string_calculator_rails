8c611305a99e13de38ecedbd6229fcc6a865654a
Prakash Sejwani <prakashsejwani@gmail.com>
2025-10-20 17:33:53 +0530

    test: add failing spec for empty string; feat: minimal add to return 0

---
+++ app/services/string_calculator.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class NegativeNumbersError < StandardError
+  def initialize(negative_numbers)
+    super("negative numbers not allowed #{negative_numbers.join(',')}")
+  end
+end
+
+class StringCalculator
+  MAX_NUMBER = 1000
+  DEFAULT_DELIMITERS = /[,
+]/.freeze
+
+  def add(numbers)
+    return 0 if numbers == ''
+    0
+  end
+end
