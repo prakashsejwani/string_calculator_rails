52a973b737a3800dbaf8b0c4ea00dd076c7be8f4
Prakash Sejwani <prakashsejwani@gmail.com>
2025-10-20 17:58:58 +0530

    feat(api): POST /string_calculators to add numbers; returns JSON result/error

---
+++ app/controllers/string_calculators_controller.rb
@@ -0,0 +1,13 @@
+class StringCalculatorsController < ApplicationController
+  protect_from_forgery with: :null_session
+
+  def create
+    input = params[:numbers].to_s
+    result = StringCalculator.new.add(input)
+    render json: { result: result }
+  rescue NegativeNumbersError => e
+    render json: { error: e.message }, status: :unprocessable_entity
+  end
+end
+
+
